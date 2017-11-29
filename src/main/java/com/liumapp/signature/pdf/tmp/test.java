560ee4cf0cd4bf7ccaa84274361b97a2daee3bfa
liumapp <liumapp.com@gmail.com>
2017-11-29 11:39:52 +0800

    add signature success

--- src/main/java/com/liumapp/signature/pdf/tmp/test.java
+++ src/main/java/com/liumapp/signature/pdf/tmp/test.java
@@ -1,7 +1,9 @@
 package com.liumapp.signature.pdf.tmp;
 
 import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfFormField;
 import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.FileOutputStream;
@@ -19,9 +21,10 @@ public class test {
         String saveName = "/usr/local/tomcat/project/working/pdf-signer/result.pdf";
         try {
             PdfReader pdfReader = new PdfReader(fileName);
-            Document document = new Document();
-            PdfWriter pdfWriter = PdfWriter.getInstance(document , new FileOutputStream(saveName));
+            PdfStamper pdfStamper = new PdfStamper(pdfReader , new FileOutputStream(saveName));
+            pdfStamper.addSignature("sign2" , 1 , 200 , 200 , 300 , 300);
             
+            pdfStamper.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
